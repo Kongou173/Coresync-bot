bd9351c76af46b92194e289036a15d8d2d942133
こんごう <jifengtongsheng@gmail.com>
2024-11-19 21:23:00 +0900

    Update script.js

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,3 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('ページが読み込まれました');
+    
+    const greetButton = document.getElementById('greetButton');
+    greetButton.addEventListener('click', () => {
+        alert('こんにちは！Botの公式サイトへようこそ！');
+    });
 });
