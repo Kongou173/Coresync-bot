d0eb4187935cecc59a6a4c6a4d9597d848425913
こんごう <jifengtongsheng@gmail.com>
2024-11-19 22:52:14 +0900

    Delete docs directory

--- docs/js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('ページが読み込まれました');
-    
-    const greetButton = document.getElementById('greetButton');
-    greetButton.addEventListener('click', () => {
-        alert('こんにちは！coresyncの公式サイトへようこそ！');
-    });
-});
